65e89e8242452048ccfd81b427c8baa9f4743e7f
TatianaKashliak <tatianakashliak@gmail.com>
2023-08-29 21:11:24 +0300

    HW-37:corrected

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import Title from "./components/Title";
 import Header from "./components/Header";
 import Tabs from "./components/Tabs/Tabs";
-import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
+    <>
+      <Header />
       <Title customClass="title">
         <span>Sign In</span>
       </Title>
-
       <Tabs />
-    </div>
+    </>
+
   );
 }
 
